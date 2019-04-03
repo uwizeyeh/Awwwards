0728aa61d37c9cdb0131523b1418ebaa14f296be
uwizeyeh <uwizeyimanahulde1@gmail.com>
2019-04-03 15:49:04 +0200

    add base.html

--- awwrds/urls.py
+++ awwrds/urls.py
@@ -1,6 +1,18 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'$', views.index, name='welcome'),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^myProfile/(\d+)', views.myProfile, name='myProfile'),
+    url(r'^project/', views.project, name='project'),
+    url(r'^images/(\d+)',views.images,name ='images'),
+    url(r'^votes/(\d+)',views.votes,name="votes"),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^api/merch/$', views.ProfileList.as_view()),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
